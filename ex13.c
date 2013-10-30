caeb20a8c096b8c8444acf19e3ebe59504e863d3
Patrick Slattery <slattery.patrickj@gmail.com>
2013-10-30 10:29:41 -0700

    upcase letter in ex13

--- ex13.c
+++ ex13.c
@@ -11,33 +11,31 @@ int main(int argc, char *argv[])
   for(i = 0; argv[1][i] != '\0'; i++) {
     char letter = argv[1][i];
 
+    if(letter > 97) {
+      letter = letter - 32;
+    }
+
     switch(letter) {
-      case 'a':
       case 'A':
         printf("%d: 'A'\n", i);
         break;
 
-      case 'e':
       case 'E':
         printf("%d 'E'\n", i);
         break;
 
-      case 'i':
       case 'I':
         printf("%d 'I'\n", i);
         break;
 
-      case 'o':
       case 'O':
         printf("%d: 'O'\n", i);
         break;
 
-      case 'u':
       case 'U':
         printf("%d: 'U'\n", i);
         break;
 
-      case 'y':
       case 'Y':
         if(i > 2) {
           printf("%d: 'Y'\n", i);
