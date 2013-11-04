653cc04df989f793b786a79e5be0b4c3e768e306
Patrick Slattery <slattery.patrickj@gmail.com>
2013-11-03 22:00:15 -0800

    add stack

--- DataStructuresAndAlgorithms/stack.c
+++ DataStructuresAndAlgorithms/stack.c
@@ -1,4 +1,47 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
+#define STACKSIZE 128
 
+typedef struct
+{
+  size_t size;
+  int items[STACKSIZE];
+} STACK;
 
+void die(const char *message)
+{
+  if(errno) {
+    perror(message);
+  } else {
+    printf("ERROR: %s\n", message);
+  }
+
+  exit(1);
+}
+
+void push(STACK *ps, int x)
+{
+  if(ps->size == STACKSIZE) {
+    die("Error: stack overflow\n");
+  } else {
+    ps->items[ps->size++] = x;
+  }
+}
+
+int pop(STACK *ps)
+{
+  if(ps->size == 0) {
+    die("Error: stack underflow\n");
+  } else {
+    return ps->items[--ps->size];
+  }
+}
+
+int main(int argc, const char *argv[])
+{
+  return 0;
+}
